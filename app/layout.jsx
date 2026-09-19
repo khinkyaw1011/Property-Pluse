967f2609d1337989e0040eb4cd96ec30b6095140
khinkyaw1011 <74442213+khinkyaw1011@users.noreply.github.com>
2026-09-19 15:03:25 +0900

    finish commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
 import { ToastContainer } from 'react-toastify';
 import { GlobalProvider } from '@/context/GlobalContext';
+import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css'
 export const metadata={
         title:'Property Pluse',
         keywords:'rentak,property,real estate',
